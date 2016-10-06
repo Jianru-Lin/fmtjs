fe3b307fb0920210c09877efa30bb69d66ab1118
Jianru Lin <jianru.lin@gmail.com>
2016-10-06 22:54:56 +0800

    支持模块解析

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var att = require('fmtjs-att')
 var render = require('fmtjs-att-render')
 
 function fmtjs(text, options) {
-	var ast = esprima.parse(text)
+	var ast = esprima.parse(text, {
+		sourceType: 'module'
+	})
 	var output = render(att(ast, options), options)
 	return output
 }
