40cbbeb13d2a3ccdb40716ffbf3fe0e334251ffe
Jianru Lin <jianru.lin@gmail.com>
2017-02-19 17:36:11 +0800

    update-version

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var render = require('fmtjs-att-render')
 
 function fmtjs(text, options) {
 	var ast = esprima.parse(text, {
-		sourceType: 'module'
+		sourceType: 'module',
+		comment: true,
+		attachComment: true
 	})
 	var output = render(att(ast, options), options)
 	return output
